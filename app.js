a9a59f13cdc3da28553c735ffb30409ca8a0982d
Vladislav <118736029+dawvvlad@users.noreply.github.com>
2023-03-01 22:26:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -137,3 +137,23 @@ function getWinner() {
         div.removeEventListener(`pointerdown`, goPlay)
     }
 }
+
+document.addEventListener('mousewheel', function(e){
+  if(!e.ctrlKey && !e.metaKey) return;
+
+  e.preventDefault();
+  e.stopImmediatePropagation();
+}, {passive:false});
+
+document.addEventListener('gesturestart', function(e){
+  e.preventDefault();
+  e.stopImmediatePropagation();
+}, {passive:false});
+
+document.addEventListener('keydown', function(e){
+  if(!e.ctrlKey && !e.metaKey) return;
+  if(e.keyCode != 189 && e.keyCode != 187) return;
+
+  e.preventDefault();
+  e.stopImmediatePropagation();
+}, {passive:false});
